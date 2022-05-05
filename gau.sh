f49c369f2d7bbb2d0b9f3d18cbce50fc6596d526
Arman <65326024+tes5hacks@users.noreply.github.com>
2022-05-05 15:49:10 -0400

    Update gau.sh

--- gau.sh
+++ gau.sh
@@ -9,13 +9,11 @@ echo $1 | gau > gau.txt
 echo "Now, running waybackurls on $1"
 echo $1 | waybackurls >> gau.txt
 
-echo "Now sorting duplicate urls"
 # Sort the duplicate urls
+echo "Now sorting duplicate urls"
 cat gau.txt | sort -u > ultimate-gau.txt
 
 # xsstool.py
-# Now, it will run xsstool on the gau results
+# Now, it will run xsstool on the gau/ waybackurls results
 echo "Now, running xsstool on $1"
 cat ultimate-gau.txt | python3 xsstool.py > reflection.txt
-
-echo "Task done, results have been saved tp reflection.txt" 
