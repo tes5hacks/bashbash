eab1c6ef716d43994f436c5b9b77a028575cf2f6
Arman <65326024+tes5hacks@users.noreply.github.com>
2022-05-05 15:44:32 -0400

    Create gau.sh

---
+++ gau.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+figlet GAU to XSStool
+
+# GAU
+echo "Now, running gau on $1"
+echo $1 | gau > gau.txt
+
+echo "Now, running gau on $1"
+echo $1 | waybackurls >> gau.txt
+
+echo "Now
+# Sort the duplicate urls
+cat gau.txt | sort -u > ultimate-gau.txt
+
+# xsstool.py
+# Now, it will run xsstool on the gau results
+echo "Now, running xsstool on $1"
+cat ultimate-gau.txt | python3 xsstool.py > reflection.txt
+
+echo "Task done, results have been saved tp reflection.txt" 
